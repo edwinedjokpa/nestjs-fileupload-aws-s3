d48c1dbccaa9f124bef9583492d4eea8f454eebf
Edwin Edjokpa <edjokpaedwin@gmail.com>
2024-05-13 15:34:53 +0100

    initial commit

---
+++ src/upload/upload.controller.ts
@@ -0,0 +1,33 @@
+import {
+  Controller,
+  FileTypeValidator,
+  MaxFileSizeValidator,
+  ParseFilePipe,
+  Post,
+  UploadedFile,
+  UseInterceptors,
+} from '@nestjs/common';
+import { FileInterceptor } from '@nestjs/platform-express';
+import { UploadService } from './upload.service';
+
+@Controller('upload')
+export class UploadController {
+  constructor(private readonly uploadService: UploadService) {}
+  @Post()
+  @UseInterceptors(FileInterceptor('file'))
+  async uploadFile(
+    @UploadedFile(
+      new ParseFilePipe({
+        validators: [
+          new MaxFileSizeValidator({ maxSize: 10000000 }),
+          new FileTypeValidator({
+            fileType: '.(jpg|jpeg|png|pdf)$',
+          }),
+        ],
+      }),
+    )
+    file: Express.Multer.File,
+  ) {
+    await this.uploadService.upload(file.originalname, file.buffer);
+  }
+}
